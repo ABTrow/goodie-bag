9e2a9a1aa1aa166b433bf1954296aa409ae52284
Austin Trow <austintrow@gmail.com>
2019-08-24 22:20:02 -0400

    candy model

--- server/db/models/Candy.js
+++ server/db/models/Candy.js
@@ -2,5 +2,23 @@ const Sequelize = require('sequelize');
 const db = require('../database');
 
 module.exports = db.define('candy', {
-  // define your model here!
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: { notEmpty: true }
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: { notEmpty: true}
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {max: 10}
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: 'https://upload.wikimedia.org/wikipedia/en/8/85/Gene_Wilder_as_Willy_Wonka.jpeg',
+  }
 });
